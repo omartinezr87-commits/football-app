e0afc4d28e076d95243f08c3d6409cdf89845051
omartinezr87-commits <omartinezr87@gmail.com>
2026-03-30 12:59:27 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'matches-v1';
+const ASSETS = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() =>
+      caches.match(e.request)
+    )
+  );
+});
